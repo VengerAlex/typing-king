a5c086d79ab3bd6fea25bd17b89d49c29a300c2b
VengerAlex <alexvenger90@gmail.com>
2023-02-12 21:08:39 +0200

    refactor paths aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     extensions: ['.ts', '.tsx'],
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: {
+      '@providers': path.resolve(__dirname, './src/components/providers'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+    }
   },
 })
