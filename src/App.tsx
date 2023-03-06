f4bd6a578789850396d214621dbbee9a8c649cc5
VengerAlex <alexvenger90@gmail.com>
2023-03-06 23:48:15 +0200

    add pages & lazy loading for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import {lazy, Suspense} from 'react'
+import {Route, Routes} from 'react-router-dom'
+import {ROUTES} from '@/helpers/types'
+
+const Home = lazy(() => import('./pages/Home'))
+const Settings = lazy(() => import('./pages/Settings'))
+const NotFound = lazy(() => import('./pages/NotFound'))
+
 export const App = () => {
   return (
-    <div className='app'>
-      <h1>Hello World</h1>
-    </div>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.SETTINGS} element={<Settings />} />
+        <Route path={ROUTES.NotFound} element={<NotFound />} />
+      </Routes>
+    </Suspense>
   )
 }
