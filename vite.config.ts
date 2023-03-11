bc69426c954661d2644746bd11f00c829b21c2aa
VengerAlex <alexvenger90@gmail.com>
2023-03-11 20:03:46 +0200

    install & setup tests in the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
+import {defineConfig, InlineConfig, UserConfig} from 'vite'
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig
+}
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/configs/setupTests.ts'],
+  },
   resolve: {alias: {'@': fileURLToPath(new URL('./src', import.meta.url))}},
-})
+} as VitestConfigExport)
